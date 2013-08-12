d75c439fabe63aa4c4eb6d91d6d66b37ff0a3f42
Dennis <dennis5943@gmail.com>
2013-08-12 13:56:13 +0800

    BETA 0.6

---
+++ SlideMenu/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SlideMenu
+//
+//  Created by Lin Dennis on 13/8/12.
+//  Copyright (c) 2013年 Lin Dennis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ SlideMenu/SecondViewController.m
@@ -0,0 +1,46 @@
+//
+//  SecondViewController.m
+//  SlideMenu
+//
+//  Created by Lin Dennis on 13/8/12.
+//  Copyright (c) 2013年 Lin Dennis. All rights reserved.
+//
+
+#import "SecondViewController.h"
+#import "ContentControllerDelegate.h"
+
+@interface SecondViewController ()<ContentControllerDelegate>
+
+-(IBAction)btnMenu:(id)sender;
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)btnMenu:(id)sender
+{
+    [self.parentViewController toggleMenu];
+}
+
+@end
